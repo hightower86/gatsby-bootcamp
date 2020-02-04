a96ea1afc9a1d58f6d3cb5e8c4981adf3616ecf2
Adel <htwr86@gmail.com>
2020-02-04 19:00:10 +0300

    styling validator-form next step

--- src/pages/project1.js
+++ src/pages/project1.js
@@ -11,15 +11,48 @@ const Project1 = () => {
         go back
       </Link>
       <div className={pStyle.container}>
-        <form id="form" className="form">
-          <div className="form-group">
+        <form id="form" className="form" autocomplete="off">
+          <div className={pStyle.formgroup}>
             <label htmlFor="name">Name</label>
-            <input type="text" name="" id="" placeholder="Enter your name" />
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Enter your name"
+            />
+            <small>Error message</small>
           </div>
-          <div className="form-group">
-            <label htmlFor="name"></label>
-            <input type="text" name="" id="" />
+          <div className={pStyle.formgroup}>
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              name="email"
+              id="email"
+              placeholder="Enter your name"
+            />
+            <small>Error message</small>
           </div>
+          <div className={pStyle.formgroup}>
+            <label htmlFor="name">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Enter password"
+            />
+            <small>Error message</small>
+          </div>
+          <div className={pStyle.formgroup}>
+            <label htmlFor="password2">Name</label>
+            <input
+              type="text"
+              name="password2"
+              id="password2"
+              placeholder="Confirm password"
+            />
+            <small>Error message</small>
+          </div>
+
           <button>Submit</button>
         </form>
       </div>
