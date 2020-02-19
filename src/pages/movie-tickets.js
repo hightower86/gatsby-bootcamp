ef4aab6ddc07aa94191e3a7ef1f76bb63deb5fbe
Adel <htwr86@gmail.com>
2020-02-19 22:08:42 +0300

    scss module to movie-tickets

--- src/pages/movie-tickets.js
+++ src/pages/movie-tickets.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from "react"
+
+import mStyle from "./movie-tickets.module.scss"
 
 const MovieTickets = () => {
   return (
-    <div>
+    <div className={mStyle.wrap}>
       <h2>Movie Tickets</h2>
     </div>
   )
