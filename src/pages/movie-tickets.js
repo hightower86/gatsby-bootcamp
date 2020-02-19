abd86a306d5dd3eb8147f8632da4661b91b253e4
Adel <htwr86@gmail.com>
2020-02-19 22:24:53 +0300

    add go back link

--- src/pages/movie-tickets.js
+++ src/pages/movie-tickets.js
@@ -1,10 +1,15 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import mStyle from "./movie-tickets.module.scss"
+import pStyle from "./form-validator.module.scss"
 
 const MovieTickets = () => {
   return (
     <div className={mStyle.wrap}>
+      <Link to="/projects" className={pStyle.back}>
+        go back
+      </Link>
       <h2>Movie Tickets</h2>
     </div>
   )
