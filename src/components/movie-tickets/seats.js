42121143f13599490e5cd24e28578b7d311d0f1d
Adel <htwr86@gmail.com>
2020-02-23 18:48:30 +0300

    problem with render rows solved

--- src/components/movie-tickets/seats.js
+++ src/components/movie-tickets/seats.js
@@ -6,13 +6,17 @@ const Seats = ({ seats }) => {
   return (
     <div className={seatsStyle.seats}>
       {seats.map(row => {
-        return row.map(seat => {
-          return (
-            <div key={seat.num} className={seatsStyle.seat}>
-              {seat.status}
-            </div>
-          )
-        })
+        return (
+          <div className={seatsStyle.row}>
+            {row.map(seat => {
+              return (
+                <div key={seat.num} className={seatsStyle.seat}>
+                  {seat.status}
+                </div>
+              )
+            })}
+          </div>
+        )
       })}
     </div>
   )
