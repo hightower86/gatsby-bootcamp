0f290d3492173fdfa280482dac298bbb64be19c1
Adel <htwr86@gmail.com>
2020-02-23 17:50:55 +0300

    add seat component and initial theater seats

--- src/components/movie-tickets/seats.js
+++ src/components/movie-tickets/seats.js
@@ -5,13 +5,14 @@ import seatsStyle from "./seats.module.scss"
 const Seats = ({ seats }) => {
   return (
     <div className={seatsStyle.seats}>
-      {//console.log(typeof seats)
-      seats.map(seat => {
-        return (
-          <div key={seat.num} className={seatsStyle.seat}>
-            {seat.status}
-          </div>
-        )
+      {seats.map(row => {
+        return row.map(seat => {
+          return (
+            <div key={seat.num} className={seatsStyle.seat}>
+              {seat.status}
+            </div>
+          )
+        })
       })}
     </div>
   )
